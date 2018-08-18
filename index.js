4bab1e31815ffd129466ac5745f93070c0c964b5
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-08-18 10:40:11 -0400

    Add functionality to disallow multi-character guesses, repeat guesses, and display guessed characters.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var word = require("./Word.js");
 
 var wordArr = ["jinxed", "waxwing", "whiffing", "foxes", "kookiest", "yammering", "jiffs", "cuffing", "vivaing", "quipping"];
 var guesses = 0;
-var incorrect = [];
+var alreadyGuessed = [];
 var theWord;
 
 function pickWord(list) {
@@ -20,24 +20,48 @@ function promptGuess() {
             name: "guess"
         }
     ]).then(function (ans) {
-        theWord.guessChar(ans.guess);
-        console.log(theWord.returnWordString() + "\n");
-        if (theWord.numGuessed == theWord.letterArr.length) {
-            console.log("You win!!");
-        }
-        else {
-            guesses++;
-            console.log(`Guesses left: ${(6 - guesses)}`);
-            if (guesses === 6) {
-                theWord.letterArr.forEach(function(i) {
-                    i.guessed = true;
-                });
-                console.log(`The word was ${theWord.returnWordString()}`);
-                console.log("Game over...");
-            }
-            else {
+        if (ans.guess.length === 1) {
+            var contains = false;
+            alreadyGuessed.forEach(function (i) {
+                if (i === ans.guess) {
+                    contains = true;
+                }
+            });
+            if (contains) {
+                console.log("You already guessed that!");
                 promptGuess();
             }
+            else {
+                alreadyGuessed.push(ans.guess);
+                var lettersGuessed = "";
+                alreadyGuessed.forEach(function(i) {
+                    lettersGuessed += i + ", ";
+                });
+                console.log(`Letters guessed: ${lettersGuessed}`);
+                theWord.guessChar(ans.guess);
+                console.log(theWord.returnWordString() + "\n");
+                if (theWord.numGuessed == theWord.letterArr.length) {
+                    console.log("You win!!");
+                }
+                else {
+                    guesses++;
+                    console.log(`Guesses left: ${(6 - guesses)}`);
+                    if (guesses === 6) {
+                        theWord.letterArr.forEach(function (i) {
+                            i.guessed = true;
+                        });
+                        console.log(`The word was ${theWord.returnWordString()}`);
+                        console.log("Game over...");
+                    }
+                    else {
+                        promptGuess();
+                    }
+                }
+            }
+        }
+        else {
+            console.log("Please guess only one character at a time.");
+            promptGuess();
         }
     });
 }
